2495ec5d376099477e5c6edf184f72347706bfad
Sam Gerstein <sgerstein@bluefinlabs.com>
2014-07-30 23:17:48 -0400

    - fix DNS resolution (getHostByName is returning 0 even on some failures; needed to check the ip to know) - add time support using NTP - add graphite support - shortened debug strings to conserve space - fix Kelvin support ;)

--- HowardStreetEnvironmentalMonitoring.ino
+++ HowardStreetEnvironmentalMonitoring.ino
@@ -3,8 +3,8 @@
 
 // Libraries
 #include <Adafruit_CC3000.h>
+#include <utility/sntp.h>
 #include <SPI.h>
-#include <string.h>
 #include "DHT.h"
 #include <stdlib.h>
 #include <Wire.h>
@@ -12,17 +12,28 @@
 #include "Secrets.h"
 #include <avr/wdt.h>
 
+// feature flags
+#define USE_NTP 1
+#define USE_THINGSPEAK 0
+#define USE_GRAPHITE 1
+
 //Define the data formats
 //One of "F", "C" or "K"
 #define UNIT_FORMAT         'F'
 //1 For true, 0 for false
-#define DEBUG_STATEMENTS    0
+#define DEBUG_STATEMENTS    1
 #if DEBUG_STATEMENTS
 #define DBG_print(x) Serial.print(x)
 #define DBG_println(x) Serial.println(x)
 #else
-#define DBG_print(x) {}
-#define DBG_println(x) {}
+#define DBG_print(x)
+#define DBG_println(x)
+#endif
+
+#if DEBUG_STATEMENTS && USE_NTP
+#define DBG_printTime(t) printTime(t)
+#else
+#define DBG_printTime(t)
 #endif
 
 //ThingSpeak will throttle at anything faster than 1 update every 15 seconds
@@ -46,14 +57,30 @@ Adafruit_TMP006 tmp006;
 DHT dht(DHTPIN, DHTTYPE);
 
 // ThingSpeak Settings
-  // Write API Key for a ThingSpeak Channel
-#define WEBSITE       "api.thingspeak.com"
+#define THINGSPEAK_WEBSITE "api.thingspeak.com"
 
+// Graphite settings
+#define GRAPHITE_HOST "test.samgerstein.net"
+#define GRAPHITE_BASE "howard-st"
+
+#if USE_GRAPHITE
+#define DATA_HOST GRAPHITE_HOST
+#else
+#define DATA_HOST THINGSPEAK_WEBSITE
+#endif
 
 // Create CC3000 instances
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT, SPI_CLOCK_DIV4); // you can change this clock speed, and you should...
-//Create a client instance                                         
-Adafruit_CC3000_Client www;
+
+#if USE_NTP
+// NTP support from CC3300; time values are EST / EDT
+sntp ntp_client = sntp(NULL, "time.nist.gov", (short)(-5 * 60), (short)(-4 * 60), true);
+SNTP_Timestamp_t last_update_time;
+// We'll update from an NTP server periodically so as not to drift:
+#define UPDATE_CLOCK_PERIOD (60 * 60 * 12)
+// NTP doesn't use Unix epoch time, it starts with 1900..
+#define NTP_TO_UNIX_SECS (((1970 - 1900) * 365 + 17) * 86400)
+#endif (USE_NTP)
 
 //Program variables
 uint32_t ip = 0;
@@ -94,7 +121,7 @@ float convertToPreferredUnits(float value) {
    if (UNIT_FORMAT == 'F') {
      value = convertCtoF(value);
    } else if (UNIT_FORMAT == 'K') {
-     value = convertCtoF(value);
+     value = convertCtoK(value);
    }
    
    return value;
@@ -116,32 +143,34 @@ float convertCtoK(float value) {
 
 void resolveServerAddress() {
  // Try looking up the website's IP address
-  DBG_print(WEBSITE);
-  DBG_println(F(" executing domain lookup."));
-    if (cc3000.getHostByName(WEBSITE, &ip) == 0) {
-      if (cc3000.getHostByName(WEBSITE, &ip) == 0) {
-        if (cc3000.getHostByName(WEBSITE, &ip) == 0) {
-          DBG_println(F("After three tries the domain name could not be resolved."));
-        }
-      }
+  DBG_print(F("lookup "));
+  DBG_println(DATA_HOST);
+
+  ip = 0;
+  while (ip == 0) {
+    cc3000.getHostByName(DATA_HOST, &ip);
+    if (ip == 0) {
+      DBG_println(F("DNS fail.. retry"));
+      delay(500);
     }
+  }
 }
 
 //Connects to the AP if the client is not already connected.
 void connectAP() {
-  DBG_println(F("Begin AP connection."));
+  DBG_println(F("Try AP.."));
   if (!cc3000.begin())
   {
-    DBG_println(F("Unable to contact device. Check the module or connection."));
+    DBG_println(F("Check WiFi!"));
   }
   
   if (!cc3000.checkConnected()) {
     // Connect to WiFi network
     cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY);
-    DBG_println(F("Connected using AES and WPA2 for security"));
+    DBG_println(F("Connected"));
   
     /* Wait for DHCP to complete */
-    DBG_println(F("Checking DHCP address allocation."));
+    DBG_println(F("Check DHCP.."));
     if (!cc3000.checkDHCP())
     {
       //wait a bit longer for the address.
@@ -149,12 +178,40 @@ void connectAP() {
     }
     
     if (cc3000.checkDHCP()) {
-      DBG_println(F("DHCP address acquired."));
+      DBG_println(F("DHCP success!"));
     } else {
-      DBG_println(F("DHCP address not acquired!"));
+      DBG_println(F("DHCP fail!"));
     }
   }
 }
+
+#if USE_NTP
+void updateTime(){
+  ntp_client.UpdateNTPTime();
+  ntp_client.NTPGetTime(&last_update_time, true);
+  DBG_printTime(&last_update_time);
+}
+
+void printTime(SNTP_Timestamp_t *sometime){
+  NetTime_t timeExtract;
+  ntp_client.ExtractNTPTime(sometime, &timeExtract);
+  Serial.print(timeExtract.hour); Serial.print(F(":")); Serial.print(timeExtract.min); Serial.print(F(":"));Serial.print(timeExtract.sec); Serial.print(F("."));Serial.println(timeExtract.millis);
+}
+
+uint32_t currentUnixTime(){
+  SNTP_Timestamp_t now;
+  ntp_client.NTPGetTime(&now, false);
+  return now.seconds - NTP_TO_UNIX_SECS;
+}
+
+void periodicUpdateTime(){
+    SNTP_Timestamp_t now;
+    ntp_client.NTPGetTime(&now, true);
+    if (now.seconds - last_update_time.seconds > UPDATE_CLOCK_PERIOD) {
+      updateTime();
+    }
+}
+#endif
 
 //Used from AvailableMemory
 int freeRam () {
@@ -169,44 +226,46 @@ void setup(void){
   DBG_println("\n\n\n");
   // Initialize
   Serial.begin(115200);
-  DBG_println(F("TMP006 Initialization Starting."));
+  DBG_println(F("Init TMP006.."));
   tmp006.begin();
-  DBG_println(F("TMP006 Initialization Complete."));
+  DBG_println(F("OK."));
   
-  DBG_println(F("TMP006 - Taking initial floor temperature."));
+  DBG_println(F("Read floor temp."));
   sampleFloorTemperature();
   DBG_print(floorTemperatureCh);
   DBG_println(UNIT_FORMAT);
   
-  DBG_println(F("AM2303/DHT22 Initialization Starting."));
+  DBG_println(F("Init AM2303.."));
   dht.begin();
-  DBG_println(F("AM2303/DHT22 Initialization Complete."));  
+  DBG_println(F("OK."));  
   
-  DBG_println(F("Taking initial air temperature."));
+  DBG_println(F("Read air temp."));
   sampleAirTemperature();
   DBG_print(airTemperatureCh);
   DBG_println(UNIT_FORMAT);
   
-  DBG_println(F("Taking initial air humidity."));
+  DBG_println(F("Read air H2O."));
   sampleAirHumidity();
   DBG_print(airHumidityCh);
   DBG_println(F(" %"));
 
-  DBG_println(F("Connecting to the Access Point."));
   connectAP();
-  DBG_println(F("Connection Complete."));
   
-  DBG_println(F("Resolving the thingspeak address"));
   resolveServerAddress();
-  DBG_println(F("Address resolved"));
   
-  DBG_print(F("Requests are made with API key: "));
+  DBG_print(F("API key: "));
   DBG_println(API_KEY);
+
+#if USE_NTP
+  DBG_println(F("Setting time"));
+  updateTime();
+#endif
 }
 
 void loop(void){
   
   delay(10000);
+  if (USE_NTP) periodicUpdateTime();
   
   if (DEBUG_STATEMENTS) startMicros = micros();
   sampleAirTemperature();
@@ -222,7 +281,8 @@ void loop(void){
   //requestString = "field1=" + airTemperatureStr + "&field2=" + airHumidityStr + "&field3=" + floorTemperatureStr;
 
   if (DEBUG_STATEMENTS) startMicros = micros();
-  updateThingSpeak();
+  if (USE_THINGSPEAK)   updateThingSpeak();
+  if (USE_GRAPHITE)     updateGraphite();
   if (DEBUG_STATEMENTS) duration = micros() - startMicros;
   
   DBG_print(F("Transmission took: "));
@@ -241,12 +301,12 @@ void updateThingSpeak()
   wdt_enable(WDTO_2S);
   
   wdt_reset();
-  www = cc3000.connectTCP(ip, 80);
+  Adafruit_CC3000_Client www = cc3000.connectTCP(ip, 80);
   wdt_reset();
   if (www.connected())
   { 
     
-    DBG_println(F("Connected to thingspeak. Forming request."));
+    DBG_println(F("Connected to thingspeak."));
     wdt_reset();    
     www.fastrprint(F("GET http://api.thingspeak.com/update?key="));
     wdt_reset();
@@ -296,7 +356,7 @@ void updateThingSpeak()
   else
   {
     wdt_disable();
-    DBG_println(F("\nConnection Failed. Attempting to reset the connection.\n"));
+    DBG_println(F("\nConnection Failed. Attempting reset.\n"));
     //Refresh the connection
     connectAP();
     resolveServerAddress();
@@ -308,4 +368,28 @@ void updateThingSpeak()
    wdt_disable();
 }
 
+void updateGraphite() {
+  Adafruit_CC3000_Client client = cc3000.connectTCP(ip, 2003);
+  const char *metrics[][2] = {{"air_temp", airTemperatureCh},
+                           {"air_h20", airHumidityCh},
+                           {"floor_temp", floorTemperatureCh}
+                          };
+  char now[12];
+  sprintf(now, "%lu", currentUnixTime());
+
+  for (int i=0; i < 3; ++i) {
+    client.fastrprint(GRAPHITE_BASE);
+    client.fastrprint(".");
+    // set GRAPHITE_HOME (eg. in your Secrets.h) to your unit number, like "20-2"
+    client.fastrprint(GRAPHITE_HOME);
+    client.fastrprint(".");
+    client.fastrprint(metrics[i][0]);
+    client.fastrprint(" ");
+    client.fastrprint(metrics[i][1]);
+    client.fastrprint(" ");
+    client.fastrprintln(now);
+  }
+  delay(100);
+  client.close();
+}
 
